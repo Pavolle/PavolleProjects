1df3a227b4dcdc6914c08a5e6728d0b528a3a8f6
alizade <agha.alizade@milko.com.tr>
2023-08-09 20:12:46 +0300

    Size manager oluşturuldu

---
+++ Pavolle.MobileCore/Common/SizeManager.cs
@@ -0,0 +1,27 @@
+﻿using Pavolle.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pavolle.MobileCore.Common
+{
+    public class SizeManager:Singleton<SizeManager>
+    {
+        private SizeManager()
+        {
+
+        }
+        double _carpan = 1;
+        public void Initialize()
+        {
+            //Yükseklik ve genişlik al. Bunlardan hangisinin büyük olşduğunu çıkar. Bu değere göre ölçekle
+        }
+
+        public double GetRealSize(double size)
+        {
+            return _carpan * size;
+        }
+    }
+}
