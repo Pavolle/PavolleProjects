55ef996032451806a4ab41fad146a34a1784c788
alizade <agha.alizade@milko.com.tr>
2023-08-09 14:29:24 +0300

    Message service için mobile app uygulaması oluşturuldu

---
+++ Pavolle.MessageService.MobileApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pavolle.MessageService.MobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
