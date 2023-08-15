8a399eeb34dd143ec6f8edc597ab3bbb68d7108b
alizade <agha.alizade@milko.com.tr>
2023-08-15 19:49:31 +0300

    Menu tasarımı tamamlandı

--- Pavolle.MessageService.WebApp/ClientApp/src/app/pages/include/menu/menu.component.ts
+++ Pavolle.MessageService.WebApp/ClientApp/src/app/pages/include/menu/menu.component.ts
@@ -8,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
 
   constructor() { }
-
+  isShowYonetim = true;
+  isShowUygulamaYonetim = true;
+  isShowUygulamaRaporlar = true;
   ngOnInit() {
+  }
+  toggleDisplayYonetim() {
+    this.isShowYonetim = !this.isShowYonetim;
+    this.isShowUygulamaYonetim=true;
+    this.isShowUygulamaRaporlar=true;
+  }
+  toggleDisplayUygulamaYonetim() {
+    this.isShowYonetim=true;
+    this.isShowUygulamaYonetim = !this.isShowUygulamaYonetim;
+    this.isShowUygulamaRaporlar=true;
+  }
+  toggleDisplayUygulamaRaporlar() {
+    this.isShowYonetim=true;
+    this.isShowUygulamaYonetim=true;
+    this.isShowUygulamaRaporlar = !this.isShowUygulamaRaporlar;
   }
 
 }
