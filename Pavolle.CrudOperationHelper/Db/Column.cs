48314ee957361587ffc888b46789b21970f1bbb1
alizade <agha.alizade@milko.com.tr>
2023-08-27 09:02:00 +0300

    Project ve table tabloları oluşturuldu

--- Pavolle.CrudOperationHelper/Db/Column.cs
+++ Pavolle.CrudOperationHelper/Db/Column.cs
@@ -7,10 +7,14 @@ using System.Threading.Tasks;
 
 namespace Pavolle.CrudOperationHelper.Db
 {
+    [Persistent("columns")]
     public class Column : BaseObject
     {
         public Column(Session session) : base(session)
         {
         }
+
+        [Persistent("table_oid")]
+        public Table Table { get; set; }
     }
 }
--- Pavolle.CrudOperationHelper/Db/Table.cs
+++ Pavolle.CrudOperationHelper/Db/Table.cs
@@ -7,10 +7,38 @@ using System.Threading.Tasks;
 
 namespace Pavolle.CrudOperationHelper.Db
 {
+    [Persistent("tables")]
     public class Table : BaseObject
     {
         public Table(Session session) : base(session)
         {
         }
+
+        [Persistent("project_oid")]
+        public Project Project { get; set; }
+
+        [Persistent("class_name")]
+        public string ClassName { get; set; }
+
+        [Persistent("db_table_name")]
+        public string DbName { get; set; }
+
+        [Persistent("list_service")]
+        public bool ListService { get; set; }
+
+        [Persistent("lookup_service")]
+        public bool LookupService { get; set; }
+
+        [Persistent("detail_service")]
+        public bool DetailService { get; set; }
+
+        [Persistent("add_service")]
+        public bool AddService { get; set; }
+
+        [Persistent("edit_service")]
+        public bool EditService { get; set; }
+
+        [Persistent("delete_service")]
+        public bool DeleteService { get; set; }
     }
 }
