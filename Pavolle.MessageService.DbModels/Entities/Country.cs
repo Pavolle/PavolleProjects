5475d92b880234f03ea16705e99f39aa4f9b5f45
alizade <agha.alizade@milko.com.tr>
2023-08-29 22:58:32 +0300

    countries tablo tasarımı tamamlandı

--- Pavolle.MessageService.DbModels/Entities/Country.cs
+++ Pavolle.MessageService.DbModels/Entities/Country.cs
@@ -7,14 +7,23 @@ using System.Threading.Tasks;
 
 namespace Pavolle.MessageService.DbModels.Entities
 {
+    [Persistent("countries")]
     public class Country : BaseObject
     {
         public Country(Session session) : base(session)
         {
         }
 
+        [Persistent("iso2")]
+        public string ISOCode2 { get; set; }
+
+        [Persistent("iso3")]
         public string ISOCode3 { get; set; }
-        public int ISOCode2 { get; set; }
+
+        [Persistent("phone_code")]
+        public string PhoneCode { get; set; }
+
+        [Persistent("name_oid")]
         public TranslateData Name { get; set; }
     }
 }
