0f483523b5122372e76a9d44d73e79ac73c46323
alizade <agha.alizade@milko.com.tr>
2023-09-01 19:37:44 +0300

    Pavolle web site

---
+++ Pavolle.FaceMemory/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pavolle.FaceMemory
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
