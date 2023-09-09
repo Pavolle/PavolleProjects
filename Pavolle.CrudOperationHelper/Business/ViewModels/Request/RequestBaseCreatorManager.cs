14ba522acac2774aa4014d9e251f5f7898714343
alizade <agha.alizade@milko.com.tr>
2023-09-10 01:04:23 +0300

    CIS Solutions web sitesi oluşturuldu

---
+++ Pavolle.CrudOperationHelper/Business/ViewModels/Request/RequestBaseCreatorManager.cs
@@ -0,0 +1,22 @@
+﻿using Pavolle.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pavolle.CrudOperationHelper.Business.ViewModels.Request
+{
+    public class RequestBaseCreatorManager : Singleton<RequestBaseCreatorManager>, ICreatorManager
+    {
+        private RequestBaseCreatorManager()
+        {
+
+        }
+
+        public bool Write(string companyName, string projectName, string projectPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
