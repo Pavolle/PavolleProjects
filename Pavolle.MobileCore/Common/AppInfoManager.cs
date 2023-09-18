2239112f663b0bace8742eba2c9473eac334a787
alizade <agha.alizade@milko.com.tr>
2023-09-18 15:34:40 +0300

    AppInfoManager about kısmı da eklendi

--- Pavolle.MobileCore/Common/AppInfoManager.cs
+++ Pavolle.MobileCore/Common/AppInfoManager.cs
@@ -1,4 +1,6 @@
-﻿using Pavolle.Core.Utils;
+﻿using Pavolle.Core.Enums;
+using Pavolle.Core.Utils;
+using Pavolle.MobileCore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,20 @@ namespace Pavolle.MobileCore.Common
         private string _appVersion;
         public void SetAppVersion(string appVersion) { _appVersion = appVersion; }
         public string GetAppVersion() { return _appVersion; }
+
+
+
+        public List<InfoModel> About { get; set; }
+        public void SetAbout(List<InfoModel> about) { About = about; }
+        public List<string> GetAbout(ELanguage language)
+        {
+            var data = About.FirstOrDefault(t => t.Language == language);
+            if(data == null)
+            {
+                return new List<string>();
+            }
+            return data.About;
+        }
 
         public string GetAppFooter() { return "© Pavolle " + DateTime.Now.Year.ToString() + " V" + _appVersion; }
     }
