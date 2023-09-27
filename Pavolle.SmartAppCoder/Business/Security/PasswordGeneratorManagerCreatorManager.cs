bcdd7d4b4afd856323d4f4af4ed9e044b158989a
alizade <agha.alizade@milko.com.tr>
2023-09-27 12:04:04 +0300

    PasswordGeneratorManager

--- Pavolle.SmartAppCoder/Business/Security/PasswordGeneratorManagerCreatorManager.cs
+++ Pavolle.SmartAppCoder/Business/Security/PasswordGeneratorManagerCreatorManager.cs
@@ -14,8 +14,27 @@ namespace Pavolle.SmartAppCoder.Business.Security
         public bool Create(string projectPath, string organizationName)
         {
             string classString = "";
+            classString += "using " + organizationName + ".Core.Utils;" + Environment.NewLine;
+            classString += "using " + organizationName + ".Security.Symmetric;" + Environment.NewLine;
             classString += "" + Environment.NewLine;
-            return FileHelperManager.Instance.WriteFile(projectPath + "\\" + organizationName + ".Security", "", "LanguageModel.cs", classString);
+            classString += "namespace " + organizationName + ".Security" + Environment.NewLine;
+            classString += "{" + Environment.NewLine;
+            classString += "    public class PasswordGeneratorManager : Singleton<PasswordGeneratorManager>" + Environment.NewLine;
+            classString += "    {" + Environment.NewLine;
+            classString += "        string _key = \"pl@gHa23\";" + Environment.NewLine;
+            classString += "" + Environment.NewLine;
+            classString += "        private PasswordGeneratorManager() { }" + Environment.NewLine;
+            classString += "" + Environment.NewLine;
+            classString += "        public string GeneratePassword(string key)" + Environment.NewLine;
+            classString += "        {" + Environment.NewLine;
+            classString += "            if (string.IsNullOrWhiteSpace(key)) return \"\";" + Environment.NewLine;
+            classString += "            var aesAlgorithm = new AESAlgorithm();" + Environment.NewLine;
+            classString += "            var aesResult = aesAlgorithm.Encrypt(key, _key);" + Environment.NewLine;
+            classString += "            return SecurityHelperManager.Instance.GetEncryptedPassword(aesResult, key).Substring(13, 24);" + Environment.NewLine;
+            classString += "        }" + Environment.NewLine;
+            classString += "    }" + Environment.NewLine;
+            classString += "}" + Environment.NewLine;
+            return FileHelperManager.Instance.WriteFile(projectPath + "\\" + organizationName + ".Security", "", "PasswordGeneratorManager.cs", classString);
 
         }
     }
