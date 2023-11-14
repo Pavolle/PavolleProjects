b171a99d5bdc9ec8d9ae280aecf42da02edc008d
alizade <agha.alizade@milko.com.tr>
2023-11-14 14:22:31 +0300

    Client kütüphanesine veriler eklendi

--- Pavolle.BES.SettingServer.ClientLib/SettingServiceManager.cs
+++ Pavolle.BES.SettingServer.ClientLib/SettingServiceManager.cs
@@ -1,7 +1,27 @@
-﻿namespace Pavolle.BES.SettingServer.ClientLib
+﻿using Pavolle.BES.SettingServer.ViewModels.Response;
+using Pavolle.Core.Utils;
+
+namespace Pavolle.BES.SettingServer.ClientLib
 {
-    public class SettingServiceManager
+    public class SettingServiceManager : Singleton<SettingServiceManager>
     {
+        public string _serviceUrl ="";
+        private SettingServiceManager() { }
+
+        public SettingsServerStatusResponse GetServerStatus()
+        {
+            if(string.IsNullOrEmpty(_serviceUrl))
+            {
+                return new SettingsServerStatusResponse
+                {
+                    ErrorMessage = "Setting Service is not initialized!"
+                };
+            }
+            else
+            {
+                return new SettingsServerStatusResponse();
+            }
+        }
 
     }
 }
