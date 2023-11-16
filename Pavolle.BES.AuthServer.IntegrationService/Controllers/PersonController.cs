d8208effa76fb2c001da796b18b3319ed2d691b4
alizade <agha.alizade@milko.com.tr>
2023-11-16 21:33:33 +0300

    Auth Server controllerlar oluşturuldu

---
+++ Pavolle.BES.AuthServer.IntegrationService/Controllers/PersonController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pavolle.BES.AuthServer.IntegrationService.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
