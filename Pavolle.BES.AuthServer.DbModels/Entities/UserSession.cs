12d427f25e2769b1be47855e3a99493eb3c040f0
alizade <agha.alizade@milko.com.tr>
2023-11-21 00:12:49 +0300

    User sessions

--- Pavolle.BES.AuthServer.DbModels/Entities/UserSession.cs
+++ Pavolle.BES.AuthServer.DbModels/Entities/UserSession.cs
@@ -7,10 +7,13 @@ using System.Threading.Tasks;
 
 namespace Pavolle.BES.AuthServer.DbModels.Entities
 {
+    [Persistent("user_sessions")]
     public class UserSession : BaseObject
     {
         public UserSession(Session session) : base(session)
         {
         }
+
+        public string AppId { get; set; }
     }
 }
