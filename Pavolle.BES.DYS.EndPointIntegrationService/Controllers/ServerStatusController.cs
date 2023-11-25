844892b92475d503c423ef8ebad003fbe905e888
alizade <agha.alizade@milko.com.tr>
2023-11-25 16:33:02 +0300

    ServerStatusController

---
+++ Pavolle.BES.DYS.EndPointIntegrationService/Controllers/ServerStatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pavolle.BES.DYS.EndPointIntegrationService.Controllers
+{
+    public class ServerStatusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
