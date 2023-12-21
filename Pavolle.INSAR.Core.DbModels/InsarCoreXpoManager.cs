e1c8ff2988b9f9f3deb8f265028566d724536dd9
alizade <agha.alizade@milko.com.tr>
2023-12-21 14:36:24 +0300

    INSAR Verilerinin indirilmesi ile ilgii süreçler

---
+++ Pavolle.INSAR.Core.DbModels/InsarCoreXpoManager.cs
@@ -0,0 +1,99 @@
+﻿using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using DevExpress.Xpo.Metadata;
+using Pavolle.Core.Utils;
+using System.Reflection;
+using Pavolle.INSAR.Core.DbModels.Entities;
+
+namespace Pavolle.INSAR.Core.DbModels
+{
+    public class InsarCoreXpoManager : Singleton<InsarCoreXpoManager>
+    {
+        private string _connectionString;
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+        }
+
+        private readonly object LockObject = new object();
+
+        private InsarCoreXpoManager()
+        {
+        }
+
+        public void InitXpo(String connectionString)
+        {
+            _connectionString = connectionString;
+            SetDataLayer();
+        }
+
+        public Session GetNewSession()
+        {
+            var session = new Session(DataLayer);
+            return session;
+        }
+
+
+        private volatile IDataLayer _fDataLayer;
+
+        private IDataLayer DataLayer
+        {
+            get
+            {
+                SetDataLayer();
+
+                return _fDataLayer;
+            }
+        }
+
+        public XPClassInfo GetClassInfo(Type t)
+        {
+            return DataLayer.Dictionary.GetClassInfo(t);
+        }
+
+        private void SetDataLayer()
+        {
+            if (_fDataLayer == null)
+            {
+                lock (LockObject)
+                {
+                    if (_fDataLayer == null)
+                    {
+                        _fDataLayer = GetDataLayer();
+                    }
+                }
+            }
+        }
+
+        public void SetDataLayer(IDataLayer dataLayer)
+        {
+            _fDataLayer = dataLayer;
+        }
+
+        private IDataLayer GetDataLayer()
+        {
+            IDataLayer dl = null;
+
+            if (!string.IsNullOrEmpty(_connectionString))
+            {
+                XpoDefault.Session = null;
+                XPDictionary dict = new ReflectionDictionary();
+
+                string connectionPoolString = XpoDefault.GetConnectionPoolString(_connectionString, 30, 500);
+
+                IDataStore store = XpoDefault.GetConnectionProvider(connectionPoolString, AutoCreateOption.DatabaseAndSchema);
+                dict.GetDataStoreSchema(typeof(BaseObject).Assembly);
+                dl = new ThreadSafeDataLayer(dict, store);
+
+                Session newSession = new Session(dl);
+                Assembly executingAssembly = Assembly.GetExecutingAssembly();
+
+                Type[] array = executingAssembly.GetTypes().Where(t => t.IsSubclassOf(typeof(BaseObject))).ToArray();
+
+                newSession.UpdateSchema(array);
+            }
+
+            return dl;
+        }
+    }
+}
---
+++ Pavolle.INSAR.Integration.CopernicusTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Pavolle.INSAR.Integration.CopernicusTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
