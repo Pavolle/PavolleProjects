2276c3e941b8ce786136f4c6832e5247e5da3c12
alizade <agha.alizade@milko.com.tr>
2023-12-24 22:56:41 +0300

    Seo uygulamaları oluşturuldu

---
+++ Pavolle.BES.SEO.MobileApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pavolle.BES.SEO.MobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
