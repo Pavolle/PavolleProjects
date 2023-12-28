ed5c236e69d25533b10601285212633a934b425f
alizade <agha.alizade@milko.com.tr>
2023-12-29 00:56:56 +0300

    Border security projesi oluşturuldu

---
+++ Pavolle.BorderSecurity.IntegrationEndpoint/Controllers/ServerStatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pavolle.BorderSecurity.IntegrationEndpoint.Controllers
+{
+    public class ServerStatusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
