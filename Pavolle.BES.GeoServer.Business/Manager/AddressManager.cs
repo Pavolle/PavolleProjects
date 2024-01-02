50310f4d04afb266c30a6e1b5b6e48a7ad52f5b8
alizade <agha.alizade@milko.com.tr>
2024-01-02 05:37:53 +0300

    Address servisleri oluşturuldu

--- Pavolle.BES.GeoServer.Business/Manager/AddressManager.cs
+++ Pavolle.BES.GeoServer.Business/Manager/AddressManager.cs
@@ -1,4 +1,9 @@
-﻿using Pavolle.Core.Utils;
+﻿using Pavolle.BES.GeoServer.ViewModels.Criteria;
+using Pavolle.BES.GeoServer.ViewModels.Request;
+using Pavolle.BES.GeoServer.ViewModels.Response;
+using Pavolle.BES.ViewModels.Request;
+using Pavolle.Core.Utils;
+using Pavolle.Core.ViewModels.Response;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +15,35 @@ namespace Pavolle.BES.GeoServer.Business.Manager
     public class AddressManager : Singleton<AddressManager>
     {
         private AddressManager() { }
+
+        public object AddAddress(AddAddressRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Delete(long? oid, IntegrationAppRequestBase request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Detail(long? oid, IntegrationAppRequestBase request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Edit(long? oid, EditAddressRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AddressListResponse List(AddressCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LookupResponse Lookup(AddressCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
