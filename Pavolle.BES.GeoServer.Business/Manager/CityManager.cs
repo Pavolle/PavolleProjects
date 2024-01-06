6d25b809c6918bb860ffcaac90eab23af25c6be6
alizade <agha.alizade@milko.com.tr>
2024-01-06 15:52:30 +0300

    City Initialize altyapısı tamamlandı

--- Pavolle.BES.GeoServer.Business/Manager/CityManager.cs
+++ Pavolle.BES.GeoServer.Business/Manager/CityManager.cs
@@ -1,13 +1,20 @@
-﻿using log4net;
+﻿using DevExpress.Xpo;
+using log4net;
+using Pavolle.BES.Common.Enums;
+using Pavolle.BES.GeoServer.DbModels.Entities;
+using Pavolle.BES.GeoServer.DbModels;
 using Pavolle.BES.GeoServer.ViewModels.Criteria;
+using Pavolle.BES.GeoServer.ViewModels.Model;
 using Pavolle.BES.GeoServer.ViewModels.Request;
 using Pavolle.BES.GeoServer.ViewModels.Response;
 using Pavolle.BES.GeoServer.ViewModels.ViewData;
+using Pavolle.BES.TranslateServer.ClientLib;
 using Pavolle.BES.ViewModels.Request;
 using Pavolle.BES.ViewModels.Response;
 using Pavolle.Core.Utils;
 using Pavolle.Core.ViewModels.Response;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,13 +25,36 @@ namespace Pavolle.BES.GeoServer.Business.Manager
     public class CityManager :  Singleton<CityManager>
     {
         static readonly ILog _log = LogManager.GetLogger(typeof(CountryManager));
-        private CityManager() 
+        ConcurrentDictionary<long?, CityCacheModel> _cities;
+        bool _isCacheInitiliaze = false;
+        private CityManager()
         {
+            _log.Debug("Initilize " + nameof(CityManager));
         }
 
         public void Initilaize()
         {
+            _cities=new ConcurrentDictionary<long?, CityCacheModel>(); 
+            using (Session session = GeoServerXpoManager.Instance.GetNewSession())
+            {
+                var dataList = session.Query<City>().ToList();
 
+                foreach (var data in dataList)
+                {
+                    var cacheData = new CityCacheModel
+                    {
+                        Oid = data.Oid,
+                        CreatedTime = data.CreatedTime,
+                        DeletedTime = data.DeletedTime,
+                        LastUpdateTime = data.LastUpdateTime,
+                        NameTranslateModel = TranslateServiceManager.Instance.GetDataByOid(data.NameTranslateDataOid, EBesAppType.GeolocationServer),
+                        CountryOid=data.Country.Oid
+                    };
+
+                    _cities.TryAdd(cacheData.Oid, cacheData);
+                }
+                _isCacheInitiliaze = true;
+            }
         }
 
         public BesAddRecordResponseBase AddCity(AddCityRequest request)
--- Pavolle.BES.GeoServer.ViewModels/Model/CityCacheModel.cs
+++ Pavolle.BES.GeoServer.ViewModels/Model/CityCacheModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pavolle.BES.TranslateServer.ViewModels.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace Pavolle.BES.GeoServer.ViewModels.Model
 {
     public class CityCacheModel
     {
+        public long Oid { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime? DeletedTime { get; set; }
+        public DateTime? LastUpdateTime { get; set; }
+        public TranslateDataCacheModel? NameTranslateModel { get; set; }
+        public long CountryOid { get; set; }
     }
 }
