7dc2cc5f116937123efba46000bb7577caf38906
alizade <agha.alizade@milko.com.tr>
2024-01-06 20:30:58 +0300

    Address cache data initialize tamamlandı

--- Pavolle.BES.GeoServer.Business/Manager/AddressManager.cs
+++ Pavolle.BES.GeoServer.Business/Manager/AddressManager.cs
@@ -1,11 +1,19 @@
-﻿using Pavolle.BES.GeoServer.ViewModels.Criteria;
+﻿using DevExpress.Xpo;
+using log4net;
+using Pavolle.BES.Common.Enums;
+using Pavolle.BES.GeoServer.DbModels.Entities;
+using Pavolle.BES.GeoServer.DbModels;
+using Pavolle.BES.GeoServer.ViewModels.Criteria;
+using Pavolle.BES.GeoServer.ViewModels.Model;
 using Pavolle.BES.GeoServer.ViewModels.Request;
 using Pavolle.BES.GeoServer.ViewModels.Response;
+using Pavolle.BES.TranslateServer.ClientLib;
 using Pavolle.BES.ViewModels.Request;
 using Pavolle.BES.ViewModels.Response;
 using Pavolle.Core.Utils;
 using Pavolle.Core.ViewModels.Response;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,11 +23,43 @@ namespace Pavolle.BES.GeoServer.Business.Manager
 {
     public class AddressManager : Singleton<AddressManager>
     {
-        private AddressManager() { }
+        static readonly ILog _log = LogManager.GetLogger(typeof(AddressManager));
+        ConcurrentDictionary<long?, AddressCacheModel> _cacheData;
+        bool _isCacheInitiliaze = false;
+        private AddressManager()
+        {
+            _log.Debug("Initilize " + nameof(AddressManager));
+        }
 
         public void Initilaize()
         {
+            _cacheData = new ConcurrentDictionary<long?, AddressCacheModel>();
+            using (Session session = GeoServerXpoManager.Instance.GetNewSession())
+            {
+                var dataList = session.Query<Address>().ToList();
 
+                foreach (var data in dataList)
+                {
+                    var cacheData = new AddressCacheModel
+                    {
+                        Oid = data.Oid,
+                        CreatedTime = data.CreatedTime,
+                        DeletedTime = data.DeletedTime,
+                        LastUpdateTime = data.LastUpdateTime,
+                        DistrictOid=data.District.Oid,
+                        CityOid = data.City.Oid,
+                        CountryOid = data.Country.Oid,
+                        Latitude=data.Latitude,
+                        Longitude=data.Longitude,
+                        OpenAddress=data.OpenAddress,
+                        StreetName=data.StreetName,
+                        ZipCode = data.ZipCode
+                    };
+
+                    _cacheData.TryAdd(cacheData.Oid, cacheData);
+                }
+                _isCacheInitiliaze = true;
+            }
         }
 
         //TODO Burada oid bilgisini geri dönmemiz gerekebilir. Çünkü bu oid bilgisi bir yerlere yazılacak.
--- Pavolle.BES.GeoServer.ViewModels/Model/AddressCacheModel.cs
+++ Pavolle.BES.GeoServer.ViewModels/Model/AddressCacheModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pavolle.BES.TranslateServer.ViewModels.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace Pavolle.BES.GeoServer.ViewModels.Model
 {
     public class AddressCacheModel
     {
+        public long CountryOid { get; set; }
+        public long? CityOid { get; set; }
+        public long? DistrictOid { get; set; }
+        public string OpenAddress { get; set; }
+        public string StreetName { get; set; }
+        public string ZipCode { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public DateTime? DeletedTime { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public long Oid { get; set; }
+        public DateTime? LastUpdateTime { get; set; }
     }
 }
